9f97b77638a4ae5f534f8ab2392002b7e502f524
Hannah Warren <hannahkwarren@gmail.com>
2021-11-09 22:20:39 -0500

    Initial commit

---
+++ spec/passenger_spec.rb
@@ -0,0 +1,14 @@
+require './lib/passenger'
+
+RSpec.describe Passenger do
+
+  before(:each) do
+    @charlie = Passenger.new({"name" => "Charlie", "age" => 18})
+    @taylor = Passenger.new({"name" => "Taylor", "age" => 12})
+  end
+
+  it 'exists' do
+    expect(@charlie).to be_instance_of(Passenger)
+    expect(@taylor).to be_instance_of(Passenger)
+  end
+end
